856d09ae65340cf65375666072663e2eca471364
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2024-04-26 18:26:46 -0300

    feat/ added functionality and interaction to the aplication

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import { Header } from './components/Header'
 import { Input } from './components/Input'
 import { Button } from './components/Button'
@@ -7,24 +9,75 @@ import {PlusCircle} from '@phosphor-icons/react'
 import styles from "./Home.module.css"
 import './global.module.css'
 
+export interface TaskType {
+  id: number;
+  text: string;
+  isCompleted: boolean;
+}
+
+
 export function Home() {
+  const [tasks, setTasks] = useState<TaskType[]>([]);
+  const [newTask, setNewTask] = useState("");
+  
+
+  function handleAddNewTask() {
+    if(!newTask) {
+      return;
+    } 
+
+    const addedTask: TaskType = {
+        id: new Date().getTime(),
+        text: newTask, 
+        isCompleted: false,
+    }   
+
+    setTasks((state) => [...state, addedTask]);
+    setNewTask("");
+  }
+
+  function handleCompletedTask(id: number) {
+    const updatedTasks = [...tasks];
+
+    updatedTasks.map((task) => 
+      task.id === id ? (task.isCompleted = !task.isCompleted) : task)      
+
+    console.log(updatedTasks)
+
+    setTasks(updatedTasks)
+  }
+
+  function handleRemovedTask(id: number) {
+    const filteredTasks = tasks.filter((task) => task.id !== id)
+
+    if(!confirm('Tem certeza que deseja apagar a tarefa?')) {
+      return;
+    }
+
+    setTasks(filteredTasks)
+  }
+
     return (
     <main className={styles.main}>
       <Header/>
       <section className={styles.content}>
         <div className={styles.inputWrapper}>
-          <Input />
-          <Button>
+          <Input 
+           name='task'
+           value={newTask}
+           onChange={(e) => setNewTask(e.target.value)}
+          />
+          <Button onClick={handleAddNewTask}>
             Criar 
             <PlusCircle size={16} color="#f2f2f2" weight='bold'/>
           </Button>        
         </div>
         <div className={styles.taskListContainer}>
           <ListHeader/>
-          <ItemList isCompleted={false}/>
-          <ItemList isCompleted={false}/>
-          <ItemList isCompleted/>
-          <ItemList isCompleted/>     
+          {tasks.map((task) => (
+            <ItemList key={task.id} task={task} isTaskComplete={handleCompletedTask} removeTask={handleRemovedTask} />
+          ))
+          }           
         </div>
       </section>
     </main>      
--- src/components/List/ItemList.tsx
+++ src/components/List/ItemList.tsx
@@ -1,26 +1,38 @@
 import styles from "./ItemList.module.css"
 import { Trash, Check } from '@phosphor-icons/react'
+import { TaskType } from "../../Home";
 
-interface ItemListProps  {
-  isCompleted: boolean;
+
+interface TaskProps  {
+  task: TaskType;
+  isTaskComplete: (id: number) => void;
+  removeTask: (id: number) => void;
 }
 
-export function ItemList({isCompleted}: ItemListProps) {
+export function ItemList({task, isTaskComplete, removeTask}: TaskProps) {
 
-  const checkboxCheckedClassName =  isCompleted ? styles['checkbox-checked'] : styles['checkbox-unchecked'];
+  function handleTaskStatus() {
+    isTaskComplete(task.id);
+  }
 
-  const isTaskCompleted = isCompleted ? styles['task-completed'] : styles['task'];
+  function handleRemove() {
+    removeTask(task.id);
+  }
+
+  const checkboxCheckedClassName =  task.isCompleted ? styles['checkbox-checked'] : styles['checkbox-unchecked'];
+
+  const isTaskCompleted = task.isCompleted ? styles['task-completed'] : styles['task'];
 
   return (
     <div className={styles.container}>
-      <label htmlFor="checkbox">
-      <input readOnly type="checkbox"/>
+      <label htmlFor="checkbox" onClick={handleTaskStatus}>
+      <input readOnly type="checkbox" checked={task.isCompleted} />
       <span className={`${styles.checkbox} ${checkboxCheckedClassName}`} >
-        {isCompleted ? <Check/> : ''}
+        {task.isCompleted ? <Check/> : ''}
       </span>
-      <p className={`${styles.task} ${isTaskCompleted}`}>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+      <p className={`${styles.task} ${isTaskCompleted}`}>{task.text}</p>
       </label>
-      <button className={styles.deleteButton}><Trash size={24} /></button>
+      <button className={styles.deleteButton} onClick={handleRemove}><Trash size={24} /></button>
     </div>
  )
 }
